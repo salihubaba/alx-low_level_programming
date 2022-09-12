d375a2924499d3bcadd8f05112ef13543d761b71
Muhammad Salihu <salihumodibo1@gmail.com>
2022-09-12 12:38:11 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+/**
+ *  main - entry point
+ *
+ *  Return: 0
+ */
+#include<stdio.h>
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
