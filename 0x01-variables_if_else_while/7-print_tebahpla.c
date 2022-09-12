b2e14ae8ab751a3bb37ed2a5e417c283818d644b
Muhammad Salihu <salihumodibo1@gmail.com>
2022-09-12 12:31:36 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+#include<stdio.h>
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
