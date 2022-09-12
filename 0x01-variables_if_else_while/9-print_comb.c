f915ffcb4d5cd7a20fbbd350c437f22b380e66e8
Muhammad Salihu <salihumodibo1@gmail.com>
2022-09-12 12:46:48 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+/**
+ * main -entry point
+ *
+ * Return: 0
+ */
+#include<stdio.h>
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
