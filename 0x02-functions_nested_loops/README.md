435b2f9f265047cf799c0bbdd3b3fdd0ac96ce15
Muhammad Salihu <salihumodibo1@gmail.com>
2022-09-13 09:07:51 -0700

    First task

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This project is for C programming functions and nested loops
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef _main_h_
+#define _main_h_
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+
+#endif
+
