e0d3513c7ca66774ea2869bc475c6b9811e33eba
Muhammad Salihu <salihumodibo1@gmail.com>
2022-09-13 12:45:58 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+/**
+ * int _isalpha(int c) - determines if the input is an alphabet
+ * @c: The character in ASCII code
+ *
+ * Return: 1 and 0
+ */
+#include "main.h"
+int _isalpha(int c)
+{
+	if ((c <= 97 && c <= 122) || (c <= 65 && c <= 90))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+	_putchar('\n');
+}
