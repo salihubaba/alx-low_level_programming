87e16f2da88d10d9e51b43145ca8077276d7ba96
Muhammad Salihu <salihumodibo1@gmail.com>
2022-09-13 09:41:23 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+/**
+ * print_alphabet_x10 - prints a-z in 10 places
+ *
+ * Return: void
+ */
+#include "main.h"
+void print_alphabet_x10(void)
+{
+	char a, b;
+
+	for (a = 0 ; a <= 9 ; a++)
+	{
+		for (b = 'a' ; b <= 'z' ; b++)
+		{
+			_putchar('b');
+		}
+		_putchar('\n');
+	}
+}
