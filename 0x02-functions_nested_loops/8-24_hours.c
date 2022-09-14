d1de652b9954e6f857f39d3321fa1a5313811c6b
Muhammad Salihu <salihumodibo1@gmail.com>
2022-09-14 07:30:28 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Return: 0
+ */
+#include "main.h"
+void jack_bauer(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 23; a++)
+	{
+		for (b = 0; b <= 59; b++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
