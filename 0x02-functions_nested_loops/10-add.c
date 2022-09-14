d25740e06f459bf9b5ce1bb36edc7ee8e45e90bb
Muhammad Salihu <salihumodibo1@gmail.com>
2022-09-14 07:44:19 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+/**
+ * add - adds two integers and returns the result
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: integer
+ */
+#include "main.h"
+int add(int a, int b)
+{
+	r = a + b;
+	return (r);
+}
+
+
