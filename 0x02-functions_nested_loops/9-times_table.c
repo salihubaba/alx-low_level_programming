a100ee8a5428b4c925662019f42e2e16e83d999d
Muhammad Salihu <salihumodibo1@gmail.com>
2022-09-14 12:38:34 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: void
+ */
+#include "main.h"
+void times_table(void)
+{
+	int c, d, p;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(48);
+		for (d = 1; d <= 9; d++)
+		{
+			p = c * d;
+			_putchar(44);
+			_putchar(32);
+			if (p <= 9)
+			{
+				_putchar(32);
+				putchar(p + 48);
+			}
+			else
+			{
+				_putchar((p / 10) + 48);
+				_putchar((p % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
