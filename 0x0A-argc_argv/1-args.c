63ca3c6842aad0cc7979d562e7952d7f7331c2de
Muhammad Salihu <salihumodibo1@gmail.com>
2022-10-05 07:16:39 -0700

    Adding 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * Return: 0 for success
  */
 #include <stdio.h>
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
