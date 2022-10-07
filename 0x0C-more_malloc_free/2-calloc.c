52d306a567d500e8ad9d2a280967f57fadc282ef
Muhammad Salihu <salihumodibo1@gmail.com>
2022-10-07 05:01:48 -0700

    Adding 2-calloc.c file

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,17 +17,16 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
-	return (NULL);
+		return (NULL);
 
-p = malloc(nmemb * size);
+	p = malloc(nmemb * size);
 
-if (p == NULL)
-	return (NULL);
+	if (p == NULL)
+		return (NULL);
 
 	for (i = 0; i < (nmemb * size); i++)
-	{
 		p[i] = 0;
-	}
+
 	return (p);
 }
 
