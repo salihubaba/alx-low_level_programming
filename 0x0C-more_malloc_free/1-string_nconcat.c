94ee57d1c27ec394f46d3ab1e31fa00b1ba2fd40
Muhammad Salihu <salihumodibo1@gmail.com>
2022-10-07 04:54:31 -0700

    Adding 2-calloc.c file

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes
+ * Return:  pointer to the allocated memory.
+ * If the function fails, it should return NULL
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	
