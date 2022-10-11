f1b6e045d688cf1344079875a307139c8c20cd6e
Muhammad Salihu <salihumodibo1@gmail.com>
2022-10-11 07:04:50 -0700

    Adding dog.h file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This project is for structures in C programming
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct dog - a structure for dog properties
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: a struct that stores information of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
