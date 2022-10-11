12f7cb06377a5ec8065b346ccabee73a3dcf7a0b
Muhammad Salihu <salihumodibo1@gmail.com>
2022-10-11 12:20:05 -0700

    Adding 5-free_dog.c file

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - function that frees dogs.
+ * @d: dog instance
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->age);
+		free(d->owner);
+	}
+}
