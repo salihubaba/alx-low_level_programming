ff3bc8fbe999208f2684c9f1828b68cdb90ae6f9
Muhammad Salihu <salihumodibo1@gmail.com>
2022-10-21 12:49:11 -0700

    a

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "list.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: head of the linked list
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n;
+
+	n = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		       printf("[%d] %s\n", 0, "(nil)");
+		else
+		        printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}	
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This is a project for linked lists in c
